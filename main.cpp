96e31fe60721995b748a8b4b4a5f211626dea51a
austingriffith94 <austgriffia@gmail.com>
2017-11-10 00:00:53 -0500

    filled in read.me and edited comments in main file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 //Austin Griffith
 //October 15, 2017
 //ISYE 6767
+//Hedging Error and Profit and Loss Calculations
 
 #include <iostream>
 #include <fstream>
@@ -19,49 +20,44 @@ using namespace std;
 
 int main()
 {
-    //Hedging Error and Profit and Loss Calculations
-
-    //initial values for part 2
-    //can change these to find values for desired dates
-    //t0 is purchase date
-    //t1 is sell date
-    //texp is the expiration date
-    //type is call "C", or put "P"
-    //q2 is dividends
-    //K2 is the strike price
-    //sigma2 is the sigma used in the vega estimation
+    //initialized values
     string t0 = "2011-07-05";
     string t1 = "2011-07-29";
     string texp = "2011-09-17";
     string type = "C";
-    double q2 = 0.0;
-    double K2 = 500.0;
-    double sigma2 = 0.24;
+    double Q = 0.0;
+    double K = 500.0;
+    double sigma = 0.24;
 
     //class reads the csv files
     reader reader;
 
-    //open and read interest.csv
+    //filenames
+    string csv_int = "interest.csv";
+    string csv_op = "op_GOOG.csv";
+    string csv_sec = "sec_GOOG.csv";
+
+    //open and read interest csv
     ifstream intfile("");
-    intfile.open("interest.csv");
+    intfile.open(csv_int);
     if(intfile.good())
     {
         reader.interest_input(intfile);
     }
     intfile.close();
 
-    //open and read op_GOOG.csv
+    //open and read options csv
     ifstream opfile("");
-    opfile.open("op_GOOG.csv");
+    opfile.open(csv_op);
     if(opfile.good())
     {
         reader.op_input(opfile);
     }
     opfile.close();
 
-    //open and read sec_GOOG.csv
+    //open and read sec prices csv
     ifstream secfile("");
-    secfile.open("sec_GOOG.csv");
+    secfile.open(csv_sec);
     if(secfile.good())
     {
         reader.sec_input(secfile);
@@ -79,18 +75,18 @@ int main()
     vector<double> price;
     for(int i = 0; i < dates.size(); i++)
     {
-        double bid = reader.bid(dates[i],texp,K2,type);
-        double offer = reader.offer(dates[i],texp,K2,type);
+        double bid = reader.bid(dates[i],texp,K,type);
+        double offer = reader.offer(dates[i],texp,K,type);
         double bo = (bid + offer)*0.5;
         price.push_back(bo);
     }
 
     //initializes hedge class
     string name = "results.csv";
-    hedge hedge2(sec,rates,price,dates,K2,reader.exp_days,sigma2,q2);
-    hedge2.volatility();
-    hedge2.error();
-    hedge2.out(name);
+    hedge hedge1(sec,rates,price,dates,K,reader.exp_days,sigma,Q);
+    hedge1.volatility();
+    hedge1.error();
+    hedge1.out(name);
 
     return 0.0;
 }
